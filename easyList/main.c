f71ccd74cc7387df8b85cf9476b0bd7ae7421445
Hanwen <hanwen.liu@tum.de>
2023-01-07 18:58:06 +0100

    update easylist

--- easyList/main.c
+++ easyList/main.c
@@ -1,10 +1,124 @@
-//
-// Created by 瀚文 on 06.01.23.
-//
-
 #include <stdio.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(){
-    printf("123");
+#define LOAD_MODE 0
+#define RUN_MODE 1
+
+// TODO: create easylist
+// Store in the disk
+
+int get_handler(char *store_path, int flag)
+{
+    // Mark to use it on XRP
+    // int fd = open(store_path, flag | O_DIRECT, 0644);
+    int fd = open(store_path, flag, 0644);
+    if (fd < 0)
+    {
+        printf("Failed to open file %s!\n", store_path);
+        exit(1);
+    }
+    return fd;
+}
+
+int initialize(size_t num, int mode, char *store_path)
+{
+    int store;
+    if (mode == LOAD_MODE)
+    {
+        store = get_handler(store_path, O_CREAT | O_TRUNC | O_WRONLY);
+    }
+    else
+    {
+        store = get_handler(store_path, O_RDONLY);
+    }
+
+    printf("In total %lu elements (Current is integers)\n", num);
+    return store;
+}
+
+/* Create a new store on disk at [store_path] with [elements]*/
+int load(size_t num, char *store_path)
+{
+    int store_fd = initialize(num, LOAD_MODE, store_path);
+
+    //    TODO: start storing the elements
+    //    Use for saving the elements;
+    int *const store_begin;
+
+    // TODO: generate array
+    //  Seed the random number generator with the current time
+    srand(time(NULL));
+
+    int array[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = rand() % num;
+    }
+
+    //    TODO:store in the store_path
+    ssize_t write_size = num * sizeof(int);
+    ssize_t bytes_writen = write(store_fd, array, write_size);
+
+    printf("write_size: %lu\n", write_size);
+    printf("bytes_writen: %lu\n", bytes_writen);
+
+    if (write_size < 0)
+    {
+        fprintf(stderr, "failure: partial write of index node\n");
+        exit(1);
+    }
+
+    close(store_fd);
+    return 0;
+}
+
+int fetch(int fd, int index)
+{
+    char buffer[sizeof(int)];
+    ssize_t num_read = pread(fd, buffer, sizeof(int), index * sizeof(int));
+    return *((int *)buffer);
+
+    // int value = *((int *)buffer);
+    // printf("Value at index %i: %d\n", index, value);
+    // return value;
+}
+
+int operate(char *store_path, int pass_in_index, int num, int iteration)
+{
+    int fd = open(store_path, O_RDONLY);
+    int index = pass_in_index;
+    int value;
+    for (int i = 0; i < iteration; i++)
+    {
+        value = fetch(fd, index);
+        index = 2 * value % num;
+    }
+    return fetch(fd, index);
+}
+
+// TODO: get function
+
+int main()
+{
+    int num = 1000000;
+    int iteration = 10000000;
+    clock_t generate_start = clock();
+    load(num, "hanwen_db");
+    clock_t generate_end = clock();
+    printf("Generating Time is %f seconds.\n", (double)(generate_end - generate_start) / CLOCKS_PER_SEC);
+
+    // int fd = open("hanwen_db", O_RDONLY);
+    // for (int i = 0; i < 1000; i++)
+    // {
+    //     fetch(fd, i);
+    // }
+    clock_t compute_start = clock();
+    int result = operate("hanwen_db", 0, num, iteration);
+    clock_t compute_end = clock();
+    printf("Result is : %d\n", result);
+    printf("Computing Time is %f seconds.\n", (double)(compute_end - compute_start) / CLOCKS_PER_SEC);
     return 0;
 }
--- easylist/helpers.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Hanwen on 06.01.23.
-//
-
-#ifndef EASYLIST_HELPERS_H
-#define EASYLIST_HELPERS_H
-
-#endif //EASYLIST_HELPERS_H
