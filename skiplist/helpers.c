52871b78689e26631516377718cb0194bc72ce7d
Hanwen <hanwen.liu@tum.de>
2023-01-11 15:30:29 +0100

    skiplist structure init

---
+++ skiplist/helpers.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "helpers.h"
+#include "db_types.h"
+
+#define BLK_SIZE 512
+#define BLK_SIZE_LOG 9
+#define SCRATCH_SIZE 4096
+
+// TODO: Need to check the number
+#define SYS_READ_XRP 450
+
+typedef unsigned long ptr__t;
+typedef unsigned long uintptr_t;
+
+#define aligned_alloca(align, size) (((uintptr_t)alloca((size) + (align)-1) + ((align)-1)) & ~(uintptr_t)((align)-1));
+
+long lookup_bpf(int db_fd, int bpf_fd, struct Query *query, ptr__t index_offset)
+{
+    /* Set up buffers and query */
+    int *buf = (int *)aligned_alloca(0x1000, 0x1000);
+    int *scratch = (int *)aligned_alloca(0x1000, SCRATCH_SIZE);
+    memset(buf, 0, 0x1000);
+    memset(scratch, 0, 0x1000);
+
+    struct ScatterGatherQuery *sgq = (struct ScatterGatherQuery *)scratch;
+    sgq->keys[0] = query->key;
+    sgq->n_keys = 1;
+
+    /* Syscall to invoke BPF */
+    long ret = syscall(SYS_READ_XRP, db_fd, buf, BLK_SIZE, index_offset, bpf_fd, scratch);
+
+    struct MaybeValue *maybe_v = &sgq->values[0];
+    query->found = (long)maybe_v->found;
+    if (query->found)
+    {
+        memcpy(query->value, maybe_v->value, sizeof(int));
+    }
+    return ret;
+}
+
+int load_bpf_program(char *path)
+{
+    struct bpf_object *obj;
+    int ret, progfd;
+
+    ret = bpf_prog_load(path, BPF_PROG_TYPE_XRP, &obj, &progfd);
+    if (ret)
+    {
+        printf("Failed to load bpf program\n");
+        exit(1);
+    }
+
+    return progfd;
+}
---
+++ skiplist/xrp-bpf/operate.c
@@ -0,0 +1,81 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <string.h>
+#include "../db_types.h"
+
+#define IN_PROCESS 0;
+#define REACH_END 1;
+
+#define EBPF_INT_SIZE 4
+
+char LICENSE[] SEC("license") = "GPL";
+
+// static __inline void set_context_next_index(struct bpf_xrp *context, struct ScatterGatherQuery *query)
+// {
+//     query->current_index += 1;
+//     query->state_flags = 0;
+//     if (query->current_index >= query->n_keys || query->current_index >= SG_KEYS)
+//     {
+//         context->done = 1;
+//         context->next_addr[0] = 0;
+//         context->size[0] = 0;
+//     }
+//     else
+//     {
+//         context->next_addr[0] = query->root_pointer;
+//         context->size[0] = BLK_SIZE;
+//     }
+// }
+
+// static __inline void set_context_next_iteration(struct bpf_xrp *context, struct ScatterGatherQuery *query)
+// {
+//     query->current_iteration += 1;
+//     query->state_flag = 0;
+//     if (query->current_iteration >= query->iteration)
+//     {
+//         context->done = 1;
+//         context->next_addr[0] = 0;
+//         context->size[0] = 0;
+//     }
+//     else
+//     {
+//         // TODO: need to check again
+//         context->next_addr[0] = 0;
+//         context->size[0] = 0;
+//     }
+// }
+
+#define EBPF_CONTEXT_MASK SG_KEYS - 1
+
+SEC("operate")
+unsigned int easylist(struct bpf_xrp *context)
+{
+    struct ScatterGatherQuery *query = (struct ScatterGatherQuery *)context->scratch;
+    unsigned int current_iteration = query->current_iteration;
+    unsigned int current_array_index = query->current_array_index;
+    // Extension to searching more elements - range
+    // int *curr_idx = &query->current_index;
+
+    // Fetch the target value
+    // TODO: find out the relating address operations
+    unsigned long location = (query->array + current_array_index * EBPF_INT_SIZE);
+    unsigned int value = *(unsigned int *)location;
+
+    // TODO: pass out the new index things - By using context->data
+    // Case 1 - End Term Processing -> fetch the value
+    // Return the value
+    if (current_iteration >= query->iteration)
+    {
+        query->state_flags = REACH_END;
+        context->done = 1;
+        // Saving the result to context->date
+        memcpy((int *)location, context->data, sizeof(int));
+    }
+
+    // Case 2 - Continue Processing
+
+    unsigned int new_index = (value * 2) % (query->array_length);
+    query->current_array_index = new_index;
+
+    return 0;
+}
